69105e47faf9d5b6a19cea5afee8b77c6f0555ec
muthuni-dealwis <kkmdealwis@gmail.com>
2024-04-29 21:37:13 +0530

    front-end updated

--- code/Web_Application/patient-clinical-letter-generator/app/register/page.tsx
+++ code/Web_Application/patient-clinical-letter-generator/app/register/page.tsx
@@ -29,23 +29,42 @@ const RegisterPage: React.FC<any> = (props) =>{
 
     return(
         <div className="auth-form-container">
-            <h2  className="login-register-heading">Register</h2>
+            <label className="login-register-heading font-sans text-slate-200 font-medium mb-9 tracking-wider">Register</label>
 
         <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
+            <label htmlFor="name" className="font-sans text-slate-200 font-normal mb-1">Full name</label>
+            <input 
+                className="h-9 bg-slate-300 opacity-95 rounded px-4 font-sans font-normal placeholder-gray-500 text-slate-800 mb-4"
+                value={name} 
+                onChange={(e) => setName(e.target.value)} 
+                id="name" 
+                placeholder="full Name"
+                name="name" />
             {/* {errors.name && <span className='text-danger'> {errors.name} </span>} */}
 
-            <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="email" className="font-sans text-slate-200 font-normal mb-1">Email</label>
+            <input 
+                className="h-9 bg-slate-300 opacity-95 rounded px-4 font-sans font-normal placeholder-gray-500 text-slate-800 mb-4" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}type="email" 
+                placeholder="youremail@gmail.com" 
+                id="email" 
+                name="email" />
             {/* {errors.email && <span className='text-danger'> {errors.email} </span>} */}
 
-            <label htmlFor="password">Password</label>
-            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <label htmlFor="password" className="font-sans text-slate-200 font-normal mb-1">Password</label>
+            <input 
+                className="h-9 bg-slate-300 opacity-95 rounded px-4 font-sans font-normal placeholder-gray-500 text-slate-800 mb-4"
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                type="password" 
+                placeholder="********" 
+                id="password" 
+                name="password" />
             {/* {errors.password && <span className='text-danger'> {errors.password} </span>} */}
 
             <Link href = "/">
-                <button className="linkSubmit" type="submit">Register</button>
+                <button  className="linkSubmit mt-10 bg-violet-500 font-sans font-medium text-white tracking-widest" type="submit">Register</button>
             </Link>  
         </form>
 
